39314346de24bc54b493882005309dfd7ed1450d
JEONG.MINKYO@nbt.com <jeong.minkyo@nbt.com>
2018-06-08 14:42:52 +0900

    table 검색기능 추가

--- app/controllers/jira_controller.rb
+++ app/controllers/jira_controller.rb
@@ -9,6 +9,58 @@ class JiraController < ApplicationController
       page = params[:page]
     end
 
-    @jira = Jira.all.order('num desc').page(page).per(LIST_PER_PAGE)
+    if params[:search].blank? && params['start_date'].blank? && params['end_date'].blank?
+      @jira = Jira.all.order('num desc').page(page).per(LIST_PER_PAGE)
+      @jira_group = Jira.all.group('assignee').count
+      @jira_date = Jira.all.group('created').count
+      @jira_month = Jira.all.group('concat(substring(created,6,2),"월")').count
+    elsif params['start_date'].blank? && params['end_date'].blank?
+      if params[:search_category].present?
+        case params[:search_category]
+          when Jira::SEARCH_CATEGORY['REQTS_num'].to_s
+            where_clause = "num like '%#{params[:search]}%'"
+            @jira = Jira.where(where_clause).order('num DESC').page(page).per(LIST_PER_PAGE)
+            @jira_group = Jira.where(where_clause).group('assignee').count
+            @jira_date = Jira.where(where_clause).group('created').count
+            @jira_month = Jira.where(where_clause).group('concat(substring(created,6,2),"월")').count
+          when Jira::SEARCH_CATEGORY['title'].to_s
+            where_clause = "title like '%#{params[:search]}%'"
+            @jira = Jira.where(where_clause).order('num DESC').page(page).per(LIST_PER_PAGE)
+            @jira_group = Jira.where(where_clause).group('assignee').count
+            @jira_date = Jira.where(where_clause).group('created').count
+            @jira_month = Jira.where(where_clause).group('concat(substring(created,6,2),"월")').count
+          when Jira::SEARCH_CATEGORY['assignee'].to_s
+            where_clause = "assignee like '%#{params[:search]}%'"
+            @jira = Jira.where(where_clause).order('num DESC').page(page).per(LIST_PER_PAGE)
+            @jira_group = Jira.where(where_clause).group('assignee').count
+            @jira_date = Jira.where(where_clause).group('created').count
+            @jira_month = Jira.where(where_clause).group('concat(substring(created,6,2),"월")').count
+          when Jira::SEARCH_CATEGORY['reporter'].to_s
+            where_clause = "reporter like '%#{params[:search]}%'"
+            @jira = Jira.where(where_clause).order('num DESC').page(page).per(LIST_PER_PAGE)
+            @jira_group = Jira.where(where_clause).group('assignee').count
+            @jira_date = Jira.where(where_clause).group('created').count
+            @jira_month = Jira.where(where_clause).group('concat(substring(created,6,2),"월")').count
+        end
+        @selected_category = params[:search_category].to_s
+      end
+    else
+      if params['start_date'].present? && params['end_date'].present?
+        start_date = Jira.change_string_to_time(params['start_date'])
+        end_date = Jira.change_string_to_time(params['end_date'])
+        @jira = Jira.where("created >= '#{start_date}' and created <= '#{end_date}'")
+                    .order('num desc').page(page).per(LIST_PER_PAGE)
+        @jira_group = Jira.where("created >= '#{start_date}' and created <= '#{end_date}'")
+                          .group('assignee').count
+        @jira_date = Jira.where("created >= '#{start_date}' and created <= '#{end_date}'")
+                         .group('created').count
+        @jira_month = Jira.where("created >= '#{start_date}' and created <= '#{end_date}'")
+                          .group('concat(substring(created,6,2),"월")').count
+      end
+    end
+
+    respond_to do |format|
+      format.html
+    end
   end
 end
--- app/models/jira.rb
+++ app/models/jira.rb
@@ -1,2 +1,13 @@
 class Jira < ApplicationRecord
+
+  SEARCH_CATEGORY = {
+      'REQTS_num' => '1',
+      'title' => '2',
+      'assignee' => '3',
+      'reporter' => '4'
+  }
+
+  def self.change_string_to_time(time_str)
+    Time.parse("#{time_str['year']}-#{time_str['month']}-#{time_str['day']}").strftime('%Y-%m-%d')
+  end
 end
