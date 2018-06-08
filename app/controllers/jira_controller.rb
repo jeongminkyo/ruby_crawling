6f03c2fb60a62fc0c5e2fc88efdae9188354b598
JEONG.MINKYO@nbt.com <jeong.minkyo@nbt.com>
2018-06-08 14:37:58 +0900

    table page 기능 추가

--- app/controllers/jira_controller.rb
+++ app/controllers/jira_controller.rb
@@ -1,6 +1,14 @@
 class JiraController < ApplicationController
 
+  LIST_PER_PAGE = 50
+
   def crawling
-    @jira = Jira.all
+    if params[:page].blank?
+      page = 1
+    else
+      page = params[:page]
+    end
+
+    @jira = Jira.all.order('num desc').page(page).per(LIST_PER_PAGE)
   end
 end
